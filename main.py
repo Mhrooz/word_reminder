03015eae1f0e8284193462b64b1117b1b7d19a7e
Mhrooz <583886027@qq.com>
2021-04-12 00:42:55 +0200

    read

---
+++ main.py
@@ -0,0 +1,9 @@
+from openpyxl import load_workbook 
+import string
+filename = 'words.xlsx'
+wb = load_workbook(filename = filename)
+range_names = 'Sheet1'
+sheet_ranges = wb[range_names]
+for column in range(1, 233):
+    for row in string.ascii_lowercase[:3]:
+        print(sheet_ranges[row+str(column)].value)
